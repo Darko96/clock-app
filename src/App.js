0cc730581836061d04a3beb1a994d645a9f3f280
Darko Mijatovic <darkomijatovic2022@gmail.com>
2024-03-03 22:44:16 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// , it's currently
+
+// in
+
+// More
+// Less
+
+// Current timezone
+// Day of the year
+// Day of the week
+// Week number
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>hello world!</div>;
 }
 
 export default App;
