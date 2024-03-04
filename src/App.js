419c42004c0bdfcf8c245084fae2baf93e585d75
Darko Mijatovic <darkomijatovic2022@gmail.com>
2024-03-04 21:07:02 +0100

    responsive design - tablet

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ export default function App() {
   const [countryCode, setCountryCode] = useState("Serbia");
 
   const [showDetails, setShowDetails] = useState(false);
+  const [shouldRefresh, setShouldRefresh] = useState(false);
 
   useEffect(function () {
     async function fetchTime() {
@@ -39,16 +40,22 @@ export default function App() {
     fetchTime();
   }, []);
 
-  useEffect(function () {
-    async function fetchQuote() {
-      const response = await fetch("https://api.quotable.io/random");
-      const data = await response.json();
+  useEffect(
+    function () {
+      async function fetchQuote() {
+        const response = await fetch("https://api.quotable.io/random");
+        const data = await response.json();
 
-      setQuote(data.content);
-      setAuthor(data.author);
-    }
-    fetchQuote();
-  }, []);
+        setQuote(data.content);
+        setAuthor(data.author);
+      }
+
+      if (shouldRefresh) fetchQuote();
+
+      fetchQuote();
+    },
+    [shouldRefresh]
+  );
 
   useEffect(function () {
     async function fetchQuote() {
@@ -67,18 +74,22 @@ export default function App() {
     setShowDetails((prevState) => !prevState);
   }
 
+  function handleRefresh() {
+    setShouldRefresh((prevState) => !prevState);
+  }
+
   return (
     <main
       style={{
-        background: `url(${dayTimeImg})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "100%",
+        backgroundImage:
+          hours >= 5 && hours < 18
+            ? `url(${dayTimeImg})`
+            : `url(${nightTimeImg})`,
       }}
     >
-      {/* half */}
       <div className={`container ${showDetails && "half"}`}>
-        <div>
-          <Quote quote={quote} author={author} />
+        <div className="box">
+          <Quote quote={quote} author={author} onRefresh={handleRefresh} />
           <Time
             hours={hours}
             minutes={minutes}
@@ -96,12 +107,12 @@ export default function App() {
   );
 }
 
-function Quote({ quote, author }) {
+function Quote({ quote, author, onRefresh }) {
   return (
     <div className="quote-container">
       <div className="quote-box">
         <p className="quote">"{quote}"</p>
-        <button className="refresh-btn">
+        <button className="refresh-btn" onClick={onRefresh}>
           <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M7.188 10.667a.208.208 0 01.147.355l-2.344 2.206a5.826 5.826 0 009.578-2.488l2.387.746A8.322 8.322 0 013.17 14.94l-2.149 2.022a.208.208 0 01-.355-.148v-6.148h6.52zm7.617-7.63L16.978.958a.208.208 0 01.355.146v6.23h-6.498a.208.208 0 01-.147-.356L13 4.765A5.825 5.825 0 003.43 7.26l-2.386-.746a8.32 8.32 0 0113.76-3.477z"
@@ -125,7 +136,12 @@ function Time({ hours, minutes, zone, city, countryCode }) {
       <div className="part-of-day">
         {hours >= 5 && hours < 12 && (
           <>
-            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              className="rotating"
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
                 d="M11.78 19.417c.594 0 1.083.449 1.146 1.026l.006.125v1.842a1.152 1.152 0 01-2.296.125l-.007-.125v-1.842c0-.636.516-1.151 1.152-1.151zM6.382 17.18a1.15 1.15 0 01.09 1.527l-.09.1-1.302 1.303a1.152 1.152 0 01-1.718-1.528l.09-.1 1.302-1.302a1.15 1.15 0 011.628 0zm12.427 0l1.303 1.303a1.15 1.15 0 11-1.628 1.627L17.18 18.81a1.15 1.15 0 111.628-1.628zM11.781 5.879a5.908 5.908 0 015.901 5.902 5.908 5.908 0 01-5.901 5.902 5.908 5.908 0 01-5.902-5.902 5.908 5.908 0 015.902-5.902zm10.63 4.75a1.151 1.151 0 110 2.303h-1.843a1.151 1.151 0 110-2.303h1.842zm-19.418 0a1.151 1.151 0 01.126 2.296l-.125.007H1.15a1.151 1.151 0 01-.125-2.296l.125-.007h1.842zm1.985-7.268l.1.09 1.303 1.302a1.151 1.151 0 01-1.528 1.718l-.1-.09L3.45 5.08A1.15 1.15 0 014.978 3.36zm15.133.09c.45.449.45 1.178 0 1.628L18.808 6.38a1.151 1.151 0 11-1.628-1.628l1.303-1.303c.449-.449 1.178-.449 1.628 0zM11.781 0c.636 0 1.151.515 1.151 1.151v1.843a1.152 1.152 0 01-2.303 0V1.15C10.63.515 11.145 0 11.781 0z"
                 fill="#FFF"
@@ -135,9 +151,14 @@ function Time({ hours, minutes, zone, city, countryCode }) {
             Good morning
           </>
         )}
-        {hours >= 12 && hours <= 18 && (
+        {hours >= 12 && hours < 18 && (
           <>
-            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              className="rotating"
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
                 d="M11.78 19.417c.594 0 1.083.449 1.146 1.026l.006.125v1.842a1.152 1.152 0 01-2.296.125l-.007-.125v-1.842c0-.636.516-1.151 1.152-1.151zM6.382 17.18a1.15 1.15 0 01.09 1.527l-.09.1-1.302 1.303a1.152 1.152 0 01-1.718-1.528l.09-.1 1.302-1.302a1.15 1.15 0 011.628 0zm12.427 0l1.303 1.303a1.15 1.15 0 11-1.628 1.627L17.18 18.81a1.15 1.15 0 111.628-1.628zM11.781 5.879a5.908 5.908 0 015.901 5.902 5.908 5.908 0 01-5.901 5.902 5.908 5.908 0 01-5.902-5.902 5.908 5.908 0 015.902-5.902zm10.63 4.75a1.151 1.151 0 110 2.303h-1.843a1.151 1.151 0 110-2.303h1.842zm-19.418 0a1.151 1.151 0 01.126 2.296l-.125.007H1.15a1.151 1.151 0 01-.125-2.296l.125-.007h1.842zm1.985-7.268l.1.09 1.303 1.302a1.151 1.151 0 01-1.528 1.718l-.1-.09L3.45 5.08A1.15 1.15 0 014.978 3.36zm15.133.09c.45.449.45 1.178 0 1.628L18.808 6.38a1.151 1.151 0 11-1.628-1.628l1.303-1.303c.449-.449 1.178-.449 1.628 0zM11.781 0c.636 0 1.151.515 1.151 1.151v1.843a1.152 1.152 0 01-2.303 0V1.15C10.63.515 11.145 0 11.781 0z"
                 fill="#FFF"
@@ -147,7 +168,7 @@ function Time({ hours, minutes, zone, city, countryCode }) {
             Good afternoon
           </>
         )}
-        {hours > 18 && (
+        {hours >= 18 && (
           <>
             <svg width="23" height="24" xmlns="http://www.w3.org/2000/svg">
               <path
@@ -197,25 +218,29 @@ function Details({ dayOfWeek, dayOfYear, weekNumber, timezone }) {
   console.log(timezone);
 
   return (
-    <div className="details-containere">
+    <div className="details-container">
       <div className="column-one">
-        <div>
-          <p>Current timezone</p>
-          <p>{timezone}</p>
+        <div className="column-one-box">
+          <p className="heading">Current timezone</p>
+          <p className="text">Europe/London</p>
+          {/* <p className="text">{timezone}</p> */}
         </div>
-        <div>
-          <p>Day of the year</p>
-          <p>{dayOfYear}</p>
+        <div className="column-one-box">
+          <p className="heading">Day of the year</p>
+          <p className="text">295</p>
+          <p className="text">{dayOfYear}</p>
         </div>
       </div>
       <div className="column-two">
-        <div>
-          <p>Day of the week</p>
-          <p>{dayOfWeek}</p>
+        <div className="column-two-box">
+          <p className="heading">Day of the week</p>
+          <p className="text">1</p>
+          {/* <p className="text">{dayOfWeek}</p> */}
         </div>
-        <div>
-          <p>Week number</p>
-          <p>{weekNumber}</p>
+        <div className="column-two-box">
+          <p className="heading">Week number</p>
+          <p className="text">42</p>
+          {/* <p className="text">{weekNumber}</p> */}
         </div>
       </div>
     </div>
