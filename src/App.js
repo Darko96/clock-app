92a01e38a0813f78f055bec4329e3db407dbebfd
Darko Mijatovic <darkomijatovic2022@gmail.com>
2024-03-07 23:34:40 +0100

    btns fixed

--- src/App.js
+++ src/App.js
@@ -112,7 +112,7 @@ export default function App() {
           />
         </div>
         <div>
-          <Button onShowDetails={handleShowDetails} />
+          <Button onShowDetails={handleShowDetails} title="show details" />
         </div>
       </div>
       {showDetails && (
@@ -132,7 +132,7 @@ function Quote({ quote, author, onRefresh, showDetails }) {
     <div className={`quote-container ${showDetails && "gone"}`}>
       <div className={`quote-box`}>
         <p className="quote">"{quote}"</p>
-        <button className="refresh-btn" onClick={onRefresh}>
+        <button className="refresh-btn" onClick={onRefresh} title="refresh">
           <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M7.188 10.667a.208.208 0 01.147.355l-2.344 2.206a5.826 5.826 0 009.578-2.488l2.387.746A8.322 8.322 0 013.17 14.94l-2.149 2.022a.208.208 0 01-.355-.148v-6.148h6.52zm7.617-7.63L16.978.958a.208.208 0 01.355.146v6.23h-6.498a.208.208 0 01-.147-.356L13 4.765A5.825 5.825 0 003.43 7.26l-2.386-.746a8.32 8.32 0 0113.76-3.477z"
@@ -237,7 +237,7 @@ function Time({
 
 function Button({ onShowDetails }) {
   return (
-    <button className="btn" onClick={onShowDetails}>
+    <button className="btn" onClick={onShowDetails} title="more">
       <span>More</span>
 
       <div className="arrow">
